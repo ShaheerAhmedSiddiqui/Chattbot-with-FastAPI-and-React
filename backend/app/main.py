4749ef22d9840a96d17aebbffc07699b50b2ec8a
Shaheer Ahmed <mrshaheer75@gmail.com>
2026-07-03 20:25:45 +0500

    fixes in main and authcontxt

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,30 @@
 from fastapi import FastAPI
-from app.database.connection import SessionLocal, engine
+from fastapi.middleware.cors import CORSMiddleware
+from app.database.connection import engine
 from app.database import database_model
 from app.api.router import api_router
+
 database_model.Base.metadata.create_all(bind=engine)
 
 
-app = FastAPI()
+app = FastAPI(title="Enterprise Chatbot API")
+
+# Ensure all variations of localhost are completely allowed
+origins = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+    expose_headers=["*"]
+)
+
 @app.get("/")
 def greet():
     return "hello from store"
